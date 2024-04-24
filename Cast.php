0a92c39105125162a34b2595c044f22dfb5f7d1a
LucasB67 <165795820+LucasB67@users.noreply.github.com>
2024-04-24 11:37:43 +0200

    Update Cast.php

--- Cast.php
+++ Cast.php
@@ -4,27 +4,31 @@ class Cast{
 
     private Actor    $_actor;
     private Role     $_role;
+    private Movie    $_movie;
 
-    function __construct(Actor $actor, Role $role){
+    function __construct(Actor $actor, Role $role, Movie $movie){
 
         $this->_actor = $actor;
         $this->_role  = $role;
+        $this->_movie = $movie;
+        $this->_actor->addCast($this);
+        $this->_role->addCast($this);
     }
 
+
+    function getActor() : Actor {
+        return $this->_actor;
+    }
+    function getRole() : Role {
+        return $this->_role;
+    }
+    function getMovie() : Movie {
+        return $this->_movie;
+    }
 
     function __toString(){
 
-        return "<i>$this->_actor dans le rôle de $this->_role</i>";
-    }
-
-    function getActor() : Actor {
-
-        return $this->_actor;
-    }
-
-    function getRole() : Role {
-
-        return $this->_role;
+        return "<i>$this->_actor dans le rôle de $this->_role.</i>";
     }
 }
 
