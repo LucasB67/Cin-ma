7347b55640f954b9b4bb121f5e8c73f243a8f8f4
LucasB67 <165795820+LucasB67@users.noreply.github.com>
2024-04-24 11:38:33 +0200

    Update Main.php

--- Main.php
+++ Main.php
@@ -2,12 +2,9 @@
 
 <?php 
 
-require "Movie.php";
-require "Genre.php";
-require "Author.php";
-require "Role.php";
-require "Actor.php";
-require "Cast.php";
+spl_autoload_register(function($class) {
+    include $class . '.php';
+});
 
 $genres = ["scienceFiction" => new Genre("Science fiction"),
            "horreur"        => new Genre("Horreur")];
@@ -22,13 +19,27 @@ $role2 = new Role("Pr Ellie Sattler");
 
 
 $movie1 = new Movie("Jurassic Park", "01-01-1993", 120, "Film sur des dinosaures.", [$genres["scienceFiction"], $genres["horreur"]], $spielberg);
-$movie1->appendCastMember(new Cast($actor1, $role1));
-$movie1->appendCastMember(new Cast($actor2, $role2));
+$movie2 = new Movie("Le Monde Perdu", "01-01-1997", 120, "Suite de Jurassic Park.", [$genres["scienceFiction"], $genres["horreur"]], $spielberg);
+
+// $movie1->setTitle("Jurassique Parc");
+
+$movie1->addCast(new Cast($actor1, $role1, $movie1));
+$movie1->addCast(new Cast($actor2, $role2, $movie1));
+$movie2->addCast(new Cast($actor1, $role1, $movie2));
+
+// $genres["scienceFiction"]->deleteMovie($movie1);
 
 echo $movie1->showInformations();
-echo "<br>".$genres["scienceFiction"]->showInformations();
+echo $genres["scienceFiction"]->showInformations();
 
 echo "<br>".$spielberg->showMovies();
+
 echo "<br>".$actor1->showMovies();
-echo "<br>".$role2->showActors();
+echo "<br>".$actor1->showRoles();
+
+echo "<br>".$role1->showActors();
+echo "<br>".$role2->showMovies();
+
+echo "<br>".$movie1->showActors();
+echo "<br>".$movie1->showRoles();
 ?>
